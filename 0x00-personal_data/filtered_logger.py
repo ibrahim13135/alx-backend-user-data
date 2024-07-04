9de4cb27f5e637b65e5e179135cd992b1dff61c9
Hima <abrahimmahmoud5454@gmail.com>
2024-07-04 23:00:16 +0300

    add filter_datum function that take password and data_of_birth

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,18 +6,30 @@ This module contains the filter_datum function for obfuscating PII fields.
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
     """
-    Returns the log message obfuscated by replacing specified fields with a redaction string.
-    
+    Returns the log message obfuscated by replacing specified fields
+    with a redaction string.
+
     Args:
-        fields (List[str]): A list of strings representing all fields to obfuscate.
-        redaction (str): A string representing by what the field will be obfuscated.
+        fields (List[str]): A list of strings representing
+        all fields to obfuscate.
+        redaction (str): A string representing by
+          what the field will be obfuscated.
         message (str): A string representing the log line.
-        separator (str): A string representing the character separating all fields in the log line.
-    
+        separator (str): A string representing the
+          character separating all fields in the log line.
+
     Returns:
         str: The obfuscated log message.
     """
     pattern = f"({'|'.join(fields)})=.*?{separator}"
-    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}{separator}", message)
+    return re.sub(
+        pattern,
+        lambda m: f"{m.group(1)}={redaction}{separator}",
+        message)
