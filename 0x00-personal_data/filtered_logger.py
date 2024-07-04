f8759bf2fabd41ce6dc12338d9918594a8ee2d6d
Hima <abrahimmahmoud5454@gmail.com>
2024-07-04 23:56:31 +0300

    task 2 Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,49 +10,26 @@ import logging
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: list):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        real_messeges = super().format(record)
-        filter_messeges = filter_datum(
-            self.fields,
-            self.REDACTION,
-            real_messeges,
-            self.SEPARATOR)
-        return filter_messeges
+        """ Returns filtered values from log records """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
-def filter_datum(
-        fields: List[str],
-        redaction: str,
-        message: str,
-        separator: str) -> str:
-    """
-    Returns the log message obfuscated by replacing specified fields
-    with a redaction string.
-
-    Args:
-        fields (List[str]): A list of strings representing
-        all fields to obfuscate.
-        redaction (str): A string representing by
-          what the field will be obfuscated.
-        message (str): A string representing the log line.
-        separator (str): A string representing the
-          character separating all fields in the log line.
-
-    Returns:
-        str: The obfuscated log message.
-    """
-    pattern = f"({'|'.join(fields)})=.*?{separator}"
-    return re.sub(
-        pattern,
-        lambda m: f"{m.group(1)}={redaction}{separator}",
-        message)
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Returns regex obfuscated log messages """
+    for field in fields:
+        message = re.sub(f'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
