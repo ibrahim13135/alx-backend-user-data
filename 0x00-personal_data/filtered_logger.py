eb10d40d0b85e7cf6906413524ee79a848346eba
Hima <abrahimmahmoud5454@gmail.com>
2024-07-04 23:45:16 +0300

    task 2 Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,58 +1,40 @@
 #!/usr/bin/env python3
-"""
-This module contains the filter_datum function for obfuscating PII fields.
-"""
-
-import re
-from typing import List
 import logging
+import re
 
 
 def filter_datum(
-        fields: List[str],
+        fields: list,
         redaction: str,
         message: str,
         separator: str) -> str:
-    """
-    Returns the log message obfuscated by replacing specified fields
-    with a redaction string.
-
-    Args:
-        fields (List[str]): A list of strings representing
-        all fields to obfuscate.
-        redaction (str): A string representing by
-          what the field will be obfuscated.
-        message (str): A string representing the log line.
-        separator (str): A string representing the
-          character separating all fields in the log line.
-
-    Returns:
-        str: The obfuscated log message.
-    """
-    pattern = f"({'|'.join(fields)})=.*?{separator}"
-    return re.sub(
-        pattern,
-        lambda m: f"{m.group(1)}={redaction}{separator}",
-        message)
+    """Return the log message obfuscated."""
+    for field in fields:
+        message = re.sub(
+            f'{field}=[^ {separator}]*',
+            f'{field}={redaction}',
+            message)
+    return message
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: list):
+        """ Initialize the formatter with specific fields to redact """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        real_messeges = super().format(record)
-        filter_messeges = filter_datum(
+        """ Format the log record to redact specified fields """
+        original_message = super().format(record)
+        redacted_message = filter_datum(
             self.fields,
             self.REDACTION,
-            real_messeges,
+            original_message,
             self.SEPARATOR)
-        return filter_messeges
+        return redacted_message
