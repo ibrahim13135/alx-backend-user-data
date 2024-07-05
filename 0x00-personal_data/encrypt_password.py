d2f576b608c2f42727fa7ba6f868eddd0ced01ab
Hima <abrahimmahmoud5454@gmail.com>
2024-07-05 03:42:57 +0300

    task 7 Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,13 +12,19 @@ def hash_password(password: str) -> bytes:
     return hashed_pass
 
 
+# def is_valid(hashed_password: bytes, password: str) -> bool:
+#     # 'utf-8' converts the string into a byte
+#     #   representation using UTF-8 encoding.
+#     # ensures that the password string
+#     #   is correctly converted into a sequence of bytes.
+#     # because:bcrypt.checkpw function expects
+#     #    both the plain text password and the
+#     #   hashed password to be in bytes.
+#     is_true = bcrypt.checkpw(password.encode('utf-8'), hash_password)
+#     return is_true
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    # 'utf-8' converts the string into a byte
-    #   representation using UTF-8 encoding.
-    # ensures that the password string
-    #   is correctly converted into a sequence of bytes.
-    # because:bcrypt.checkpw function expects
-    #    both the plain text password and the
-    #   hashed password to be in bytes.
-    is_true = bcrypt.checkpw(password.encode('utf-8'), hash_password)
-    return is_true
+    """Implement an is_valid function
+    """
+    validity = bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    return validity
