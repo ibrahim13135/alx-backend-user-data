c298734caa8e9f9df00a9def533dedb2e227b281
Hima <abrahimmahmoud5454@gmail.com>
2024-07-05 03:44:13 +0300

    task 7 Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -13,6 +13,8 @@ def hash_password(password: str) -> bytes:
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Implement an is_valid function
+    """
     # 'utf-8' converts the string into a byte
     #   representation using UTF-8 encoding.
     # ensures that the password string
