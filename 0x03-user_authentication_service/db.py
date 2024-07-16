b6b29456e15c172043b647780ddea2d382afe36c
Hima <abrahimmahmoud5454@gmail.com>
2024-07-16 23:56:45 +0300

    solve task 2 Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,6 +34,22 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
+        
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a user to the database
+
+        Args:
+            email (str): The user's email
+            hashed_password (str): The user's hashed password
+
+        Returns:
+            User: The created user
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user        
+
     def find_user_by(self, **kwargs) -> User:
         """ Find user by a given attribute
             Args:
