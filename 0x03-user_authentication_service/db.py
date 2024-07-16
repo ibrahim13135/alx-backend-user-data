fafb20f2d20f7c46b5ea4cf14da5037c44e9d4f8
Hima <abrahimmahmoud5454@gmail.com>
2024-07-16 23:59:24 +0300

    solve task 2 Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
+"""DB module
 """
-DB module
-"""
-from sqlalchemy import create_engine, tuple_
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -20,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -34,41 +32,40 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-        
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a user to the database
+        """Add a new user to the database.
 
         Args:
-            email (str): The user's email
-            hashed_password (str): The user's hashed password
+            email (str): The user's email address.
+            hashed_password (str): The user's hashed password.
 
         Returns:
-            User: The created user
+            User: The newly created User object.
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
-        return user        
-
-    def find_user_by(self, **kwargs) -> User:
-        """ Find user by a given attribute
-            Args:
-                - Dictionary of attributes to use as search
-                  parameters
-            Return:
-                - User object
-        """
-
-        attrs, vals = [], []
-        for attr, val in kwargs.items():
-            if not hasattr(User, attr):
-                raise InvalidRequestError()
-            attrs.append(getattr(User, attr))
-            vals.append(val)
-
-        session = self._session
-        query = session.query(User)
-        user = query.filter(tuple_(*attrs).in_([tuple(vals)])).first()
-        if not user:
-            raise NoResultFound()
         return user
+
+    def find_user_by(self, **args) -> User:
+        """
+        Find a user in the database based on input arguments.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments to filter the query.
+
+        Returns:
+            User: The first matching User object.
+
+        Raises:
+            NoResultFound: If no results are found.
+            InvalidRequestError: If invalid query arguments are passed.
+        """
+        try:
+            user = self._session.query(User).filter_by(**args).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except InvalidRequestError:
+            self._session.rollback()
+            raise
